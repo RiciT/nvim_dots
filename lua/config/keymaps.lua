958d34ea30b40a7a85dc902d35d81058553714ac
RiciT <tamas.balint.farago@gmail.com>
2026-05-21 19:28:16 +0200

    added easier resizing keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,6 +17,11 @@ map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase Window Width" })
 
+map("n", "<C-E>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-N>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-M>", "<cmd>vertical resize +2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-I>", "<cmd>vertical resize -2<cr>", { desc = "Increase Window Width" })
+
 -- This part is the accomodate colemak-dh
 -- dont skip over wrapped lines when jumping lines
 local modes = { "n", "v", "o" }
